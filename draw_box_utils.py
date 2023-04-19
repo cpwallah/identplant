51292a430ae2f3d52b497f31241a59a4666e8aee
tah9 <690818882@qq.com>
2023-04-19 22:25:24 +0800

    去除冗余

--- draw_box_utils.py
+++ draw_box_utils.py
@@ -34,76 +34,9 @@ STANDARD_COLORS = [
 ]
 
 
-def draw_text(draw,
-              box: list,
-              cls: int,
-              score: float,
-              category_index: dict,
-              color: str,
-              font: str = 'arial.ttf',
-              font_size: int = 24):
-    """
-    将目标边界框和类别信息绘制到图片上
-    """
-    try:
-        font = ImageFont.truetype(font, font_size)
-    except IOError:
-        font = ImageFont.load_default()
-
-    left, top, right, bottom = box
-    # If the total height of the display strings added to the top of the bounding
-    # box exceeds the top of the image, stack the strings below the bounding box
-    # instead of above.
-
-    # {category_index[str(cls)]}
-    # display_str = f"{category_index[str(cls)]}: {int(100 * score)}%"
-    display_str = f"{category_index[str(cls)]}"
-    # display_str_heights = [font.getsize(ds)[1] for ds in display_str]
-    # Each display_str has a top and bottom margin of 0.05x.
-    # display_str_height = (1 + 2 * 0.05) * max(display_str_heights)
-
-    # if top > display_str_height:
-    #     text_top = top - display_str_height
-    #     text_bottom = top
-    # else:
-    #     text_top = bottom
-    #     text_bottom = bottom + display_str_height
-
-    # font_path = os.path.join("assets", "Hiragino Sans GB.ttc")
-
-    # local_font = ImageFont.truetype("simsun", size=font_size, encoding="gbk")
-    # for ds in display_str:
-    #     # text_width, text_height = font.getsize(ds)
-    #     # text_width = text_width * 1.5
-    #     # margin = np.ceil(0.05 * text_width)
-    #     # draw.rectangle([(left, text_top),
-    #     #                 (left + text_width + 2 * margin, text_bottom)], fill=color)
-    #     # draw.text((left + margin, text_top),
-    #     #           ds,
-    #     #           fill='black',
-    #     #           font=local_font)
-    #     left += text_width
-    return display_str
-
-
-def draw_masks(image, masks, colors, thresh: float = 0.7, alpha: float = 0.5):
-    np_image = np.array(image)
-    masks = np.where(masks > thresh, True, False)
-
-    # colors = np.array(colors)
-    img_to_draw = np.copy(np_image)
-    # TODO: There might be a way to vectorize this
-    for mask, color in zip(masks, colors):
-        img_to_draw[mask] = color
-
-    out = np_image * (1 - alpha) + img_to_draw * alpha
-    return fromarray(out.astype(np.uint8))
-
-
-
 # RGB格式颜色转换为16进制颜色格式
 def RGB_to_Hex(rgb):
-    RGB = [rgb[0],rgb[1],rgb[2]]           # 将RGB格式划分开来
+    RGB = [rgb[0], rgb[1], rgb[2]]  # 将RGB格式划分开来
     color = '#'
     for i in RGB:
         num = int(i)
@@ -151,6 +84,9 @@ def draw_objs(image: Image,
     boxes = boxes[idxs]
     classes = classes[idxs]
     scores = scores[idxs]
+    print(classes)
+    print(scores)
+
     if masks is not None:
         masks = masks[idxs]
     if len(boxes) == 0:
@@ -159,60 +95,14 @@ def draw_objs(image: Image,
 
     colors = [ImageColor.getrgb(STANDARD_COLORS[cls % len(STANDARD_COLORS)]) for cls in classes]
 
-    # print(boxes)
-    # if draw_boxes_on_image:
-    #     # draw = ImageDraw.Draw(image)
-    #     for box, cls, score, color in zip(boxes, classes, scores, colors):
-    #         left, top, right, bottom = box
-    #         maxScore = maxScore if maxScore > score else score
-    #         index = index + 1
-    # 绘制目标边界框
-    # draw.line([(left, top), (left, bottom), (right, bottom),
-    #            (right, top), (left, top)], width=line_thickness, fill=color)
-    # # 绘制类别和概率信息
-    # draw_text(draw, box.tolist(), int(cls), float(score), category_index, color, font, font_size)
-    # print(index)
-    # draw = ImageDraw.Draw(image)
-    # box = boxes[index]
-    # left, top, right, bottom = box
-    # score = scores[index]
-    # color = colors[index]
-    # cls = classes[index]
-    #
-    # # 绘制目标边界框
-    # draw.line([(left, top), (left, bottom), (right, bottom),
-    #            (right, top), (left, top)], width=line_thickness, fill=color)
-    # # 绘制类别和概率信息
-    # draw_text(draw, box.tolist(), int(cls), float(score), category_index, color, font, font_size)
-    # image = draw_masks(image, masks, colors, mask_thresh)
-    tempIndex = 0
     result_str = ''
     result_color = None
     if draw_boxes_on_image:
-        # Draw all boxes onto image.
-        draw = ImageDraw.Draw(image)
         for box, cls, score, color in zip(boxes, classes, scores, colors):
-            # if tempIndex == index:
-            # left, top, right, bottom = box
-            left = 0
-            top = 0
-            right = image.width - line_thickness
-            bottom = image.height - line_thickness
-            # print(box.tolist())
-            boxList = [left, 2 + font_size, right, bottom]
-            print(boxList)
-            # 绘制目标边界框
-            draw.line([(left, top), (left, bottom), (right, bottom),
-                       (right, top), (left, top)], width=line_thickness, fill=color)
-            # 绘制类别和概率信息
-
             # draw_text(draw, box.tolist(), int(cls), float(score), category_index, color, font, font_size)
-            result_str = draw_text(draw, boxList, int(cls), float(score), category_index, color, font, font_size)
+            result_str = f"{category_index[str(cls)]}"
             result_color = color
             break
         # tempIndex = tempIndex + 1
-    if draw_masks_on_image and (masks is not None):
-        # Draw all mask onto image.
-        image = draw_masks(image, masks, colors, mask_thresh)
 
     return [result_str, RGB_to_Hex(result_color)]
